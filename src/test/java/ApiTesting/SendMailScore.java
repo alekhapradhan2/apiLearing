86af007d5e01ca04a59103ff276b36e2d20a56a6
alekhapradhan2 <80783908+alekhapradhan2@users.noreply.github.com>
2025-04-12 23:32:25 +0530

    Update SendMailScore.java

--- src/test/java/ApiTesting/SendMailScore.java
+++ src/test/java/ApiTesting/SendMailScore.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
